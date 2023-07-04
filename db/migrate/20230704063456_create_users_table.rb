06a2f9e1ed2efc07dc3b9f5094eae053230774d4
rgabriel01 <raymond.m.gabriel@gmail.com>
2023-07-04 15:18:30 +0800

    adds migration for the users table

---
+++ db/migrate/20230704063456_create_users_table.rb
@@ -0,0 +1,12 @@
+class CreateUsersTable < ActiveRecord::Migration[7.0]
+  def up
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.timestamps
+    end
+
+    add_index :users, :email
+  end
+end
