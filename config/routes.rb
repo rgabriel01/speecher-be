d298bc6679371f5430e57697daedefcb18e82e28
rgabriel01 <raymond.m.gabriel@gmail.com>
2023-07-07 22:47:10 +0800

    forgot about the route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :speeches, only: %i[index create update], defaults: { format: 'json' }
+  resources :users, only: %i[index], defaults: { format: 'json' }
 
   get '/speeches/fetch_by_author/:author_id', to: 'speeches#fetch_by_author', as: :fetch_by_author, defaults: { format: 'json' }
   get '/speeches/search', to: 'speeches#search', as: :search, defaults: { format: 'json' }
